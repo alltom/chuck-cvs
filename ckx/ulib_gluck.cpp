cc961579a3d4365fea63320b7df593f49b3a8ee8
gw-chuck <gw-chuck>
2004-06-22 17:42:13 +0000

    *** empty log message ***

--- ckx/ulib_gluck.cpp
+++ ckx/ulib_gluck.cpp
@@ -36,7 +36,7 @@
 #if defined(__MACOSX_CORE__)
   #include <OpenGL/gl.h>
   #include <OpenGL/glu.h>
-  #include <OpenGL/glut.h>
+  #include <GLUT/glut.h>
 #else
   #include <GL/gl.h>
   #include <GL/glu.h>
--- ckx/ulib_gluck.h
+++ ckx/ulib_gluck.h
@@ -28,6 +28,7 @@
 //
 // author: Ge Wang (gewang@cs.princeton.edu)
 //         Perry R. Cook (prc@cs.princeton.edu)
+//         Philip L. Davidson (philipd@alumni.princeton.edu)
 // date: Spring 2004
 //-----------------------------------------------------------------------------
 #ifndef __ULIB_GLUCK_H__
