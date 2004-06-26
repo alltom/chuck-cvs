1e425ba261830bbf52f752dae9319d3d80ef595d
gw-chuck <gw-chuck>
2004-06-26 08:50:23 +0000

    *** empty log message ***

--- src/util_sndfile.c
+++ src/util_sndfile.c
@@ -1,7 +1,55 @@
-#include "ugen_sndfile.h"
-#include <stdio.h>
+/*----------------------------------------------------------------------------
+    ChucK Concurrent, On-the-fly Audio Programming Language
+      Compiler and Virtual Machine
 
-#include <stdlib.h>
+    Copyright (c) 2004 Ge Wang and Perry R. Cook.  All rights reserved.
+      http://chuck.cs.princeton.edu/
+      http://soundlab.cs.princeton.edu/
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+    U.S.A.
+-----------------------------------------------------------------------------*/
+
+/*
+** libsndfile Copyright (C) 1999-2004 Erik de Castro Lopo <erikd@mega-nerd.com>
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU Lesser General Public License as published by
+** the Free Software Foundation; either version 2.1 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU Lesser General Public License for more details.
+**
+** You should have received a copy of the GNU Lesser General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+*/
+
+//-----------------------------------------------------------------------------
+// name: util_sndfile.c
+// desc: libsndfile for ChucK
+//
+// authors: Ge Wang (gewang@cs.princeton.edu)
+//          Perry R. Cook (prc@cs.princeton.edu)
+//          Ari Lazier (alazier@alumni.princeton.edu)
+// libsndfile: Erik de Castro Lopo (erikd@mega-nerd.com)
+//-----------------------------------------------------------------------------
+#include "util_sndfile.h"
 
 /*
  * Copyright 1992 by Jutta Degener and Carsten Bormann, Technische
