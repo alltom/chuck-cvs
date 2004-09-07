bc4e61ddb635fb2edc89c07fee55c748b0ae177b
pld-chuck <pld-chuck>
2004-09-07 17:49:29 +0000

    more oscillators:

--- src/ugen_osc.cpp
+++ src/ugen_osc.cpp
@@ -39,7 +39,6 @@
 #define TWO_PI (2.0 * 3.14159265358979323846)
 static t_CKUINT g_srate = 0;
 
-
 //-----------------------------------------------------------------------------
 // name: osc_query()
 // desc: ...
@@ -49,19 +48,82 @@ DLL_QUERY osc_query( Chuck_DL_Query * QUERY )
     // srate
     g_srate = QUERY->srate;
 
+	//! phasor
+	QUERY->ugen_add( QUERY, "phasor", NULL );
+    // set funcs
+    QUERY->ugen_func( QUERY, osc_ctor, osc_dtor, osc_tick, osc_pmsg );
+    // add ctrls / cgets
+    QUERY->ugen_ctrl( QUERY, osc_ctrl_freq, osc_cget_freq, "float", "freq" );  //! oscillator frequency ( Hz ) 
+    QUERY->ugen_ctrl( QUERY, osc_ctrl_sfreq, osc_cget_freq, "float", "sfreq" );  //! oscillator frequency ( Hz ) , phase-matched
+    QUERY->ugen_ctrl( QUERY, osc_ctrl_phase_offset, osc_cget_phase_offset, "float", "phase_offset" ); //! phase offset 
+    QUERY->ugen_ctrl( QUERY, osc_ctrl_phase, osc_cget_phase, "float", "phase" ); //! current phase
+    QUERY->ugen_ctrl( QUERY, osc_ctrl_sync, osc_cget_sync, "int", "sync" ); //! sync to global ( 1 ) or self ( 0 ) 
+    QUERY->ugen_ctrl( QUERY, osc_ctrl_width, osc_cget_width, "float", "width" ); //! sync to global ( 1 ) or self ( 0 ) 
+
+
     //! sine oscillator
     //! (examples/osc.ck)
     QUERY->ugen_add( QUERY, "sinosc", NULL );
     // set funcs
-    QUERY->ugen_func( QUERY, sinosc_ctor, sinosc_dtor, sinosc_tick, sinosc_pmsg );
-    // add ctrl
-    QUERY->ugen_ctrl( QUERY, sinosc_ctrl_freq, sinosc_cget_freq, "float", "freq" );  //! oscillator frequency ( Hz ) 
-    QUERY->ugen_ctrl( QUERY, sinosc_ctrl_sfreq, sinosc_cget_freq, "float", "sfreq" );  //! oscillator frequency ( Hz ) , phase-matched
-    QUERY->ugen_ctrl( QUERY, sinosc_ctrl_phase_offset, sinosc_cget_phase_offset, "float", "phase_offset" ); //! phase offset 
+    QUERY->ugen_func( QUERY, osc_ctor, osc_dtor, sinosc_tick, osc_pmsg );
+    // add ctrls / cgets
+    QUERY->ugen_ctrl( QUERY, osc_ctrl_freq, osc_cget_freq, "float", "freq" );  //! oscillator frequency ( Hz ) 
+    QUERY->ugen_ctrl( QUERY, osc_ctrl_sfreq, osc_cget_freq, "float", "sfreq" );  //! oscillator frequency ( Hz ) , phase-matched
+    QUERY->ugen_ctrl( QUERY, osc_ctrl_phase_offset, osc_cget_phase_offset, "float", "phase_offset" ); //! phase offset 
     QUERY->ugen_ctrl( QUERY, sinosc_ctrl_phase, sinosc_cget_phase, "float", "phase" ); //! current phase
-    QUERY->ugen_ctrl( QUERY, sinosc_ctrl_sync, sinosc_cget_sync, "int", "sync" ); //! sync to global ( 1 ) or self ( 0 ) 
+    QUERY->ugen_ctrl( QUERY, osc_ctrl_sync, osc_cget_sync, "int", "sync" ); //! sync to global ( 1 ) or self ( 0 ) 
 
-    return TRUE;
+	//! pulse oscillators
+	QUERY->ugen_add( QUERY, "pulseosc", NULL );
+    // set funcs
+    QUERY->ugen_func( QUERY, osc_ctor, osc_dtor, pulseosc_tick, osc_pmsg );
+    // add ctrls / cgets
+    QUERY->ugen_ctrl( QUERY, osc_ctrl_freq, osc_cget_freq, "float", "freq" );  //! oscillator frequency ( Hz ) 
+    QUERY->ugen_ctrl( QUERY, osc_ctrl_sfreq, osc_cget_freq, "float", "sfreq" );  //! oscillator frequency ( Hz ) , phase-matched
+    QUERY->ugen_ctrl( QUERY, osc_ctrl_phase_offset, osc_cget_phase_offset, "float", "phase_offset" ); //! phase offset 
+    QUERY->ugen_ctrl( QUERY, osc_ctrl_phase, osc_cget_phase, "float", "phase" ); //! current phase
+    QUERY->ugen_ctrl( QUERY, osc_ctrl_sync, osc_cget_sync, "int", "sync" ); //! sync to global ( 1 ) or self ( 0 ) 
+    QUERY->ugen_ctrl( QUERY, osc_ctrl_width, osc_cget_width, "float", "width" ); //! sync to global ( 1 ) or self ( 0 ) 
+
+	//! square wave oscillator ( secretly just pulse )
+	QUERY->ugen_add( QUERY, "squareosc", NULL );
+    // set funcs
+    QUERY->ugen_func( QUERY, osc_ctor, osc_dtor, squareosc_tick, osc_pmsg );
+    // add ctrls / cgets
+    QUERY->ugen_ctrl( QUERY, osc_ctrl_freq, osc_cget_freq, "float", "freq" );  //! oscillator frequency ( Hz ) 
+    QUERY->ugen_ctrl( QUERY, osc_ctrl_sfreq, osc_cget_freq, "float", "sfreq" );  //! oscillator frequency ( Hz ) , phase-matched
+    QUERY->ugen_ctrl( QUERY, osc_ctrl_phase_offset, osc_cget_phase_offset, "float", "phase_offset" ); //! phase offset 
+    QUERY->ugen_ctrl( QUERY, osc_ctrl_phase, osc_cget_phase, "float", "phase" ); //! current phase
+    QUERY->ugen_ctrl( QUERY, osc_ctrl_sync, osc_cget_sync, "int", "sync" ); //! sync to global ( 1 ) or self ( 0 ) 
+    QUERY->ugen_ctrl( QUERY, osc_ctrl_width, osc_cget_width, "float", "width" ); //! sync to global ( 1 ) or self ( 0 ) 
+
+	//! triangle wave oscillator
+	QUERY->ugen_add( QUERY, "triosc", NULL );
+    // set funcs
+    QUERY->ugen_func( QUERY, osc_ctor, osc_dtor, triosc_tick, osc_pmsg );
+    // add ctrls / cgets
+    QUERY->ugen_ctrl( QUERY, osc_ctrl_freq, osc_cget_freq, "float", "freq" );  //! oscillator frequency ( Hz ) 
+    QUERY->ugen_ctrl( QUERY, osc_ctrl_sfreq, osc_cget_freq, "float", "sfreq" );  //! oscillator frequency ( Hz ) , phase-matched
+    QUERY->ugen_ctrl( QUERY, osc_ctrl_phase_offset, osc_cget_phase_offset, "float", "phase_offset" ); //! phase offset 
+    QUERY->ugen_ctrl( QUERY, osc_ctrl_phase, osc_cget_phase, "float", "phase" ); //! current phase
+    QUERY->ugen_ctrl( QUERY, osc_ctrl_sync, osc_cget_sync, "int", "sync" ); //! sync to global ( 1 ) or self ( 0 ) 
+    QUERY->ugen_ctrl( QUERY, osc_ctrl_width, osc_cget_width, "float", "width" ); //! sync to global ( 1 ) or self ( 0 ) 
+	
+	//! sawtooth wave oscillator
+	QUERY->ugen_add( QUERY, "sawosc", NULL );
+    // set funcs
+    QUERY->ugen_func( QUERY, osc_ctor, osc_dtor, sawosc_tick, osc_pmsg );
+    // add ctrls / cgets
+    QUERY->ugen_ctrl( QUERY, osc_ctrl_freq, osc_cget_freq, "float", "freq" );  //! oscillator frequency ( Hz ) 
+    QUERY->ugen_ctrl( QUERY, osc_ctrl_sfreq, osc_cget_freq, "float", "sfreq" );  //! oscillator frequency ( Hz ) , phase-matched
+    QUERY->ugen_ctrl( QUERY, osc_ctrl_phase_offset, osc_cget_phase_offset, "float", "phase_offset" ); //! phase offset 
+    QUERY->ugen_ctrl( QUERY, osc_ctrl_phase, osc_cget_phase, "float", "phase" ); //! current phase
+    QUERY->ugen_ctrl( QUERY, osc_ctrl_sync, osc_cget_sync, "int", "sync" ); //! sync to global ( 1 ) or self ( 0 ) 
+    QUERY->ugen_ctrl( QUERY, osc_ctrl_width, osc_cget_width, "float", "width" ); //! sync to global ( 1 ) or self ( 0 ) 
+
+
+
+	return TRUE;
 }
 
 
@@ -79,17 +141,19 @@ struct Osc_Data
     double phase_offset;
     int    sync; 
     t_CKUINT srate;
+	double width; 
     
     Osc_Data( )
     {
         t = 0.0;
         num = 0.0;
         freq = 220.0;
-        sync = 0;
+        sync = 0; //internal 
+		width = 0.5;
         phase_offset = 0.0;
         srate = g_srate;
-        sinosc_ctrl_freq( 0, this, &freq );
-        sinosc_ctrl_phase_offset( 0, this, &phase_offset );
+        osc_ctrl_freq( 0, this, &freq );
+        osc_ctrl_phase_offset( 0, this, &phase_offset );
     }
 };
 
@@ -98,7 +162,7 @@ struct Osc_Data
 // name: sinosc_ctor()
 // desc: ...
 //-----------------------------------------------------------------------------
-UGEN_CTOR sinosc_ctor( t_CKTIME now )
+UGEN_CTOR osc_ctor( t_CKTIME now )
 {
     // return data to be used later
     return new Osc_Data;
@@ -108,10 +172,10 @@ UGEN_CTOR sinosc_ctor( t_CKTIME now )
 
 
 //-----------------------------------------------------------------------------
-// name: sinosc_dtor()
+// name: osc_dtor()
 // desc: ...
 //-----------------------------------------------------------------------------
-UGEN_DTOR sinosc_dtor( t_CKTIME now, void * data )
+UGEN_DTOR osc_dtor( t_CKTIME now, void * data )
 {
     // delete
     delete (Osc_Data *)data;
@@ -124,15 +188,16 @@ UGEN_DTOR sinosc_dtor( t_CKTIME now, void * data )
 // name: sinosc_tick()
 // desc: ...
 //-----------------------------------------------------------------------------
-UGEN_TICK sinosc_tick( t_CKTIME now, void * data, SAMPLE in, SAMPLE * out )
+UGEN_TICK osc_tick( t_CKTIME now, void * data, SAMPLE in, SAMPLE * out )
 {
     Osc_Data * d = (Osc_Data *)data;
     //phase offsets don't mean so much when oscillators are keeping 
     //track of their own ticks, unless they are created at the same time..
 
-    if ( d->sync )  d->t = (double) now;
-
-    *out = (SAMPLE)sin( d->phase_offset + d->t * d->num );
+    if ( d->sync == 1 )  d->t = (double) now;
+	float ph = ( d->sync == 2 ) ? in : d->phase_offset + d->t * d->num;
+	ph -= floor ( ph );
+    *out = (SAMPLE) ph;
  
     if ( !d->sync ) d->t += 1.0;
 
@@ -140,17 +205,103 @@ UGEN_TICK sinosc_tick( t_CKTIME now, void * data, SAMPLE in, SAMPLE * out )
 }
 
 
+//-----------------------------------------------------------------------------
+// name: sinosc_tick()
+// desc: ...
+//-----------------------------------------------------------------------------
+UGEN_TICK sinosc_tick( t_CKTIME now, void * data, SAMPLE in, SAMPLE * out )
+{
+    Osc_Data * d = (Osc_Data *)data;
+    //phase offsets don't mean so much when oscillators are keeping 
+    //track of their own ticks, unless they are created at the same time..
 
+    if ( d->sync == 1 )  d->t = (double) now;
+
+	*out = (SAMPLE) ( d->sync == 2 )? sin ( TWO_PI * in ) \
+									: sin ( TWO_PI * ( d->phase_offset + d->t * d->num ) );
+ 
+    if ( !d->sync ) d->t += 1.0;
+
+    return TRUE;
+}
+
+//-----------------------------------------------------------------------------
+// name: sinosc_tick()
+// desc: ...
+//-----------------------------------------------------------------------------
+UGEN_TICK triosc_tick( t_CKTIME now, void * data, SAMPLE in, SAMPLE * out )
+{
+    Osc_Data * d = (Osc_Data *)data;
+    //phase offsets don't mean so much when oscillators are keeping 
+    //track of their own ticks, unless they are created at the same time..
+
+    if ( d->sync == 1 )  d->t = (double) now;
+
+	float ph = ( d->sync == 2 ) ? in : d->phase_offset + d->t * d->num;
+	ph -= floor ( ph );
+	float width = d->width;
+	if ( ph < width ) *out = ( width == 0.0 ) ? 1.0 :  -1.0 + 2.0 * ph / width;
+	else *out = ( width == 1.0 ) ? 0 : 1.0 - 2.0 * (ph - width) / (1.0 - width);
+	
+    if ( !d->sync ) d->t += 1.0;
+
+    return TRUE;
+}
+
+
+//-----------------------------------------------------------------------------
+// name: pulseosc_tick()
+// desc: ...
+//-----------------------------------------------------------------------------
+UGEN_TICK pulseosc_tick( t_CKTIME now, void * data, SAMPLE in, SAMPLE * out )
+{
+    Osc_Data * d = (Osc_Data *)data;
+    //phase offsets don't mean so much when oscillators are keeping 
+    //track of their own ticks, unless they are created at the same time..
+
+    if ( d->sync == 1 )  d->t = (double) now;
+
+	float ph = ( d->sync == 2 ) ? in : d->phase_offset + d->t * d->num;
+	ph -= floor ( ph );
+	if ( ph < d->width ) *out = -1.0;
+	else *out = 1.0;
+	
+    if ( !d->sync ) d->t += 1.0;
+
+    return TRUE;
+}
+
+//-----------------------------------------------------------------------------
+// name: pulseosc_tick()
+// desc: ...
+//-----------------------------------------------------------------------------
+UGEN_TICK squareosc_tick( t_CKTIME now, void * data, SAMPLE in, SAMPLE * out )
+{
+    Osc_Data * d = (Osc_Data *)data;
+	d->width = 0.5;
+	return pulseosc_tick ( now, data, in , out );
+}
+
+//-----------------------------------------------------------------------------
+// name: pulseosc_tick()
+// desc: ...
+//-----------------------------------------------------------------------------
+UGEN_TICK sawosc_tick( t_CKTIME now, void * data, SAMPLE in, SAMPLE * out )
+{
+    Osc_Data * d = (Osc_Data *)data;
+	d->width = ( d->width < 0.5 ) ? 0.0 : 1.0;
+	return triosc_tick ( now, data, in , out );
+}
 
 //-----------------------------------------------------------------------------
 // name: sinosc_ctrl_freq()
 // desc: set oscillator frequency
 //-----------------------------------------------------------------------------
-UGEN_CTRL sinosc_ctrl_freq( t_CKTIME now, void * data, void * value )
+UGEN_CTRL osc_ctrl_freq( t_CKTIME now, void * data, void * value )
 {
     Osc_Data * d = (Osc_Data *)data;
     d->freq = GET_CK_FLOAT(value);
-    d->num = TWO_PI * d->freq / d->srate;
+    d->num = d->freq / d->srate;
 }
 
 
@@ -159,22 +310,22 @@ UGEN_CTRL sinosc_ctrl_freq( t_CKTIME now, void * data, void * value )
 // desc: sets oscillator frequency, implicitly adjusts phase offset to match
 //       avoid signal discontinuity. - pld
 //-----------------------------------------------------------------------------
-UGEN_CTRL sinosc_ctrl_sfreq( t_CKTIME now, void * data, void * value )
+UGEN_CTRL osc_ctrl_sfreq( t_CKTIME now, void * data, void * value )
 {
     //phase matching freq adjust
     Osc_Data * d = (Osc_Data *)data;
     double curnum = d->num;
     d->freq = (float)GET_CK_FLOAT(value);
-    d->num = TWO_PI * d->freq / d->srate;
+    d->num = d->freq / d->srate;
     double nphase = d->phase_offset + d->t * ( curnum - d->num );
-    d->phase_offset = fmod( nphase, TWO_PI ) ; //nphase - TWO_PI * floor ( nphase / TWO_PI );
+    d->phase_offset = nphase - floor ( nphase );
 }
 
 //-----------------------------------------------------------------------------
 // name: sinosc_cget_freq()
 // desc: ...
 //-----------------------------------------------------------------------------
-UGEN_CGET sinosc_cget_freq( t_CKTIME now, void * data, void * out )
+UGEN_CGET osc_cget_freq( t_CKTIME now, void * data, void * out )
 {
     Osc_Data * d = (Osc_Data *)data;
     SET_NEXT_FLOAT( out, (t_CKFLOAT)d->freq );
@@ -186,7 +337,7 @@ UGEN_CGET sinosc_cget_freq( t_CKTIME now, void * data, void * out )
 // name: sinosc_ctrl_phase_offset()
 // desc: explicitly set oscillator phase-offset   -pld
 //-----------------------------------------------------------------------------
-UGEN_CTRL sinosc_ctrl_phase_offset( t_CKTIME now, void * data, void * value )
+UGEN_CTRL osc_ctrl_phase_offset( t_CKTIME now, void * data, void * value )
 {
     Osc_Data * d = (Osc_Data *)data;
     d->phase_offset = (float)GET_CK_FLOAT(value);
@@ -196,7 +347,7 @@ UGEN_CTRL sinosc_ctrl_phase_offset( t_CKTIME now, void * data, void * value )
 // name: sinosc_cget_phase_offset()
 // desc: ...
 //-----------------------------------------------------------------------------
-UGEN_CGET sinosc_cget_phase_offset( t_CKTIME now, void * data, void * out )
+UGEN_CGET osc_cget_phase_offset( t_CKTIME now, void * data, void * out )
 {
     Osc_Data * d = (Osc_Data *)data;
     SET_NEXT_FLOAT( out, (t_CKFLOAT)d->phase_offset );
@@ -205,53 +356,100 @@ UGEN_CGET sinosc_cget_phase_offset( t_CKTIME now, void * data, void * out )
 
 
 //-----------------------------------------------------------------------------
-// name: sinosc_ctrl_phase()
+// name: osc_ctrl_phase()
+// desc: explicitly set oscillator phase   -pld
+//-----------------------------------------------------------------------------
+UGEN_CTRL osc_ctrl_phase ( t_CKTIME now, void * data, void * value )
+{
+    Osc_Data * d = (Osc_Data *)data;
+    t_CKFLOAT phase = GET_CK_FLOAT(value);
+    double cphase =  phase - ( d->t * d->num );
+	d->phase_offset = cphase - floor ( cphase );
+}
+
+//-----------------------------------------------------------------------------
+// name: osc_cget_phase()
+// desc: ...
+//-----------------------------------------------------------------------------
+UGEN_CGET osc_cget_phase ( t_CKTIME now, void * data, void * out )
+{
+    Osc_Data * d = (Osc_Data *)data;
+    SET_NEXT_FLOAT( out, (t_CKFLOAT) d->phase_offset + d->t * d->num );
+}
+
+
+//-----------------------------------------------------------------------------
+// name: osc_ctrl_phase()
 // desc: explicitly set oscillator phase   -pld
 //-----------------------------------------------------------------------------
 UGEN_CTRL sinosc_ctrl_phase ( t_CKTIME now, void * data, void * value )
 {
     Osc_Data * d = (Osc_Data *)data;
-    t_CKFLOAT phase = GET_CK_FLOAT(value);
+    t_CKFLOAT phase = GET_CK_FLOAT(value) / TWO_PI;
     double cphase =  phase - ( d->t * d->num );
-    d->phase_offset = fmod ( cphase, TWO_PI ); //cphase - TWO_PI * floor ( cphase / TWO_PI );
+	d->phase_offset = cphase - floor ( cphase );
 }
 
 //-----------------------------------------------------------------------------
-// name: sinosc_cget_phase()
+// name: osc_cget_phase()
 // desc: ...
 //-----------------------------------------------------------------------------
 UGEN_CGET sinosc_cget_phase ( t_CKTIME now, void * data, void * out )
 {
     Osc_Data * d = (Osc_Data *)data;
-    SET_NEXT_FLOAT( out, (t_CKFLOAT) d->phase_offset + d->t * d->num );
+    SET_NEXT_FLOAT( out, (t_CKFLOAT) TWO_PI * ( d->phase_offset + d->t * d->num ) );
 }
 
 //-----------------------------------------------------------------------------
 // name: sinosc_ctrl_sync()
 // desc: set sync   -pld
 //-----------------------------------------------------------------------------
-UGEN_CTRL sinosc_ctrl_sync ( t_CKTIME now, void * data, void * value )
+UGEN_CTRL osc_ctrl_sync ( t_CKTIME now, void * data, void * value )
 {
     Osc_Data * d = (Osc_Data *)data;
     d->sync = GET_CK_INT(value);
 }
 
+
 //-----------------------------------------------------------------------------
 // name: sinosc_cget_sync()
 // desc: get sync
 //-----------------------------------------------------------------------------
-UGEN_CGET sinosc_cget_sync ( t_CKTIME now, void * data, void * out )
+UGEN_CGET osc_cget_sync ( t_CKTIME now, void * data, void * out )
 {
     Osc_Data * d = (Osc_Data *)data;
     SET_NEXT_INT( out, d->sync );
 }
+
+//-----------------------------------------------------------------------------
+// name: sinosc_cget_width()
+// desc: get sync
+//-----------------------------------------------------------------------------
+UGEN_CGET osc_cget_width ( t_CKTIME now, void * data, void * out )
+{
+    Osc_Data * d = (Osc_Data *)data;
+    SET_NEXT_FLOAT( out, d->width );
+}
+
+
+//-----------------------------------------------------------------------------
+// name: sinosc_ctrl_width()
+// desc: set sync   -pld
+//-----------------------------------------------------------------------------
+UGEN_CTRL osc_ctrl_width ( t_CKTIME now, void * data, void * value )
+{
+    Osc_Data * d = (Osc_Data *)data;
+    d->width = GET_CK_FLOAT(value);
+    d->width -= floor ( d->width );
+}
+
 
 
 //-----------------------------------------------------------------------------
 // name: sinosc_pmsg()
 // desc: ...
 //-----------------------------------------------------------------------------
-UGEN_PMSG sinosc_pmsg( t_CKTIME now, void * data, const char * msg, void * value )
+UGEN_PMSG osc_pmsg( t_CKTIME now, void * data, const char * msg, void * value )
 {
     Osc_Data * d = (Osc_Data *)data;
     if( !strcmp( msg, "print" ) )
--- src/ugen_osc.h
+++ src/ugen_osc.h
@@ -41,22 +41,32 @@
 DLL_QUERY osc_query( Chuck_DL_Query * query );
 
 // sinosc
-UGEN_CTOR sinosc_ctor( t_CKTIME now );
-UGEN_DTOR sinosc_dtor( t_CKTIME now, void * data );
-UGEN_TICK sinosc_tick( t_CKTIME now, void * data, SAMPLE in, SAMPLE * out );
-UGEN_TICK sinosc_pmsg( t_CKTIME now, void * data, const char * msg, void * value );
-UGEN_CTRL sinosc_ctrl_freq( t_CKTIME now, void * data, void * value );
-UGEN_CGET sinosc_cget_freq( t_CKTIME now, void * data, void * out );
+UGEN_CTOR osc_ctor( t_CKTIME now );
+UGEN_DTOR osc_dtor( t_CKTIME now, void * data );
+UGEN_TICK osc_tick( t_CKTIME now, void * data, SAMPLE in, SAMPLE * out );
+UGEN_TICK osc_pmsg( t_CKTIME now, void * data, const char * msg, void * value );
+UGEN_CTRL osc_ctrl_freq( t_CKTIME now, void * data, void * value );
+UGEN_CGET osc_cget_freq( t_CKTIME now, void * data, void * out );
 //sfreq &  phase controls  - pld
-UGEN_CTRL sinosc_ctrl_sfreq( t_CKTIME now, void * data, void * value );
+UGEN_CTRL osc_ctrl_sfreq( t_CKTIME now, void * data, void * value );
+UGEN_CTRL osc_ctrl_phase( t_CKTIME now, void * data, void * value );
+UGEN_CGET osc_cget_phase( t_CKTIME now, void * data, void * out );
+UGEN_CTRL osc_ctrl_phase_offset( t_CKTIME now, void * data, void * value );
+UGEN_CGET osc_cget_phase_offset( t_CKTIME now, void * data, void * out );
+UGEN_CTRL osc_ctrl_sync( t_CKTIME now, void * data, void * value );
+UGEN_CGET osc_cget_sync( t_CKTIME now, void * data, void * out );
+UGEN_CTRL osc_ctrl_width( t_CKTIME now, void * data, void * value );
+UGEN_CGET osc_cget_width( t_CKTIME now, void * data, void * out );
+
+UGEN_TICK sinosc_tick( t_CKTIME now, void * data, SAMPLE in, SAMPLE * out );
 UGEN_CTRL sinosc_ctrl_phase( t_CKTIME now, void * data, void * value );
 UGEN_CGET sinosc_cget_phase( t_CKTIME now, void * data, void * out );
-UGEN_CTRL sinosc_ctrl_phase_offset( t_CKTIME now, void * data, void * value );
-UGEN_CGET sinosc_cget_phase_offset( t_CKTIME now, void * data, void * out );
-UGEN_CTRL sinosc_ctrl_sync( t_CKTIME now, void * data, void * value );
-UGEN_CGET sinosc_cget_sync( t_CKTIME now, void * data, void * out );
 
+UGEN_TICK triosc_tick( t_CKTIME now, void * data, SAMPLE in, SAMPLE * out );
+UGEN_TICK sawosc_tick( t_CKTIME now, void * data, SAMPLE in, SAMPLE * out );
 
+UGEN_TICK pulseosc_tick( t_CKTIME now, void * data, SAMPLE in, SAMPLE * out );
+UGEN_TICK squareosc_tick( t_CKTIME now, void * data, SAMPLE in, SAMPLE * out );
 
 
 #endif
