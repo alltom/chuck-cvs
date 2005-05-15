d8e307feb2470dccd357dfefa5258723cee91402
gw-chuck <gw-chuck>
2005-05-15 01:37:04 +0000

    *** empty log message ***

--- v2/chuck_type.h
+++ v2/chuck_type.h
@@ -358,7 +358,7 @@ public:
       this->func = rhs.func;
       this->id = rhs.id;
       this->info = rhs.info;
-      this->info->add_ref(); // HACK:
+      if( this->info ) this->info->add_ref(); // HACK:
       this->name = rhs.name;
       this->owner = rhs.owner;
       this->parent = rhs.parent;
