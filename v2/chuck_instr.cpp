3fbe1864a8621624229a2d212fa945ec0ae9b84f
gw-chuck <gw-chuck>
2005-05-24 11:17:26 +0000

    *** empty log message ***

--- v2/chuck_instr.cpp
+++ v2/chuck_instr.cpp
@@ -80,13 +80,14 @@ void Chuck_Instr_Inc_int::execute( Chuck_VM * vm, Chuck_VM_Shred * shred )
 {
     t_CKBYTE *& mem_sp = (t_CKBYTE *&)shred->mem->sp;
     t_CKINT **& reg_sp = (t_CKINT **&)shred->reg->sp;
+	t_CKINT *&  the_sp = (t_CKINT *&)shred->reg->sp;
 
 	// pointer
 	pop_( reg_sp, 1 );
     // increment value
     (**(reg_sp))++;
 	// value
-	push_( (t_CKINT *&)reg_sp, **(reg_sp) );
+	push_( the_sp, **(reg_sp) );
 }
 
 
@@ -101,13 +102,14 @@ void Chuck_Instr_Dec_int::execute( Chuck_VM * vm, Chuck_VM_Shred * shred )
 {
     t_CKBYTE *& mem_sp = (t_CKBYTE *&)shred->mem->sp;
     t_CKINT **& reg_sp = (t_CKINT **&)shred->reg->sp;
+	t_CKINT *&  the_sp = (t_CKINT *&)shred->reg->sp;
 
 	// pointer
 	pop_( reg_sp, 1 );
     // decrement value
     (**(reg_sp))--;
 	// value
-	push_( (t_CKUINT *&)reg_sp, **(reg_sp) );
+	push_( the_sp, **(reg_sp) );
 }
 
 
