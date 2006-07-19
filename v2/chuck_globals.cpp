59f376f438fb0bf0ab943d384edee48998a14e3e
gw-chuck <gw-chuck>
2006-07-19 01:20:01 +0000

    *** empty log message ***

--- v2/chuck_globals.cpp
+++ v2/chuck_globals.cpp
@@ -40,7 +40,7 @@
 
 
 // current version
-const char CK_VERSION[] = "1.2.0.6-rc3 (dracula)";
+const char CK_VERSION[] = "1.2.0.6-rc4 (dracula)";
 
 // global virtual machine
 Chuck_VM * g_vm = NULL;
