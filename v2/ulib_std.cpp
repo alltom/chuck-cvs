7bbf9167821b29069a0184cdecace3dc13e34b22
gw-chuck <gw-chuck>
2007-03-16 02:21:55 +0000

    *** empty log message ***

--- v2/ulib_std.cpp
+++ v2/ulib_std.cpp
@@ -57,6 +57,13 @@ int setenv( const char *n, const char *v, int i )
 
 #endif
 
+// for ConsoleInput and StringTokenizer
+#include <sstream>
+#include <iostream>
+#include "util_thread.h"
+using namespace std;
+
+
 // KBHit
 CK_DLL_CTOR( KBHit_ctor );
 CK_DLL_MFUN( KBHit_on );
@@ -68,28 +75,6 @@ CK_DLL_MFUN( KBHit_getchar );
 CK_DLL_MFUN( KBHit_can_wait );
 
 static t_CKUINT KBHit_offset_data = 0;
-
-#ifdef AJAY
-
-#include <sstream>
-#include <iostream>
-#include <fstream>
-#include "util_thread.h"
-using namespace std;
-
-// VCR functions
-CK_DLL_CTOR( VCR_ctor );
-CK_DLL_MFUN( VCR_load );
-CK_DLL_MFUN( VCR_reset );
-CK_DLL_MFUN( VCR_seek );
-CK_DLL_MFUN( VCR_more );
-CK_DLL_MFUN( VCR_curr );
-CK_DLL_MFUN( VCR_next );
-CK_DLL_MFUN( VCR_pos );
-CK_DLL_MFUN( VCR_size );
-CK_DLL_MFUN( VCR_name );
-
-static t_CKUINT VCR_offset_data = 0;
 
 // Skot functions
 CK_DLL_CTOR( Skot_ctor );
@@ -113,6 +98,24 @@ CK_DLL_MFUN( StrTok_get2 );
 CK_DLL_MFUN( StrTok_size );
 
 static t_CKUINT StrTok_offset_data = 0;
+
+#ifdef AJAY
+
+#include <fstream>
+
+// VCR functions
+CK_DLL_CTOR( VCR_ctor );
+CK_DLL_MFUN( VCR_load );
+CK_DLL_MFUN( VCR_reset );
+CK_DLL_MFUN( VCR_seek );
+CK_DLL_MFUN( VCR_more );
+CK_DLL_MFUN( VCR_curr );
+CK_DLL_MFUN( VCR_next );
+CK_DLL_MFUN( VCR_pos );
+CK_DLL_MFUN( VCR_size );
+CK_DLL_MFUN( VCR_name );
+
+static t_CKUINT VCR_offset_data = 0;
 
 // Cereal functions
 CK_DLL_CTOR( Cereal_ctor );
@@ -291,6 +294,96 @@ DLL_QUERY libstd_query( Chuck_DL_Query * QUERY )
     KBHitManager::init();
 
 
+    // register deprecate
+    type_engine_register_deprecate( env, "Skot", "ConsoleInput" );
+
+    // begin class (Skot)
+    if( !type_engine_import_class_begin( env, "ConsoleInput", "Event",
+                                         env->global(), Skot_ctor ) )
+        return FALSE;
+
+    // add member variable
+    Skot_offset_data = type_engine_import_mvar( env, "int", "@Skot_data", FALSE );
+    if( Skot_offset_data == CK_INVALID_OFFSET ) goto error;
+
+    // add prompt()
+    func = make_new_mfun( "Event", "prompt", Skot_prompt );
+    if( !type_engine_import_mfun( env, func ) ) goto error;
+
+    // add prompt()
+    func = make_new_mfun( "Event", "prompt", Skot_prompt2 );
+    func->add_arg( "string", "what" );
+    if( !type_engine_import_mfun( env, func ) ) goto error;
+
+    // add ready()
+    func = make_new_mfun( "int", "more", Skot_more );
+    if( !type_engine_import_mfun( env, func ) ) goto error;
+
+    // add getString()
+    func = make_new_mfun( "string", "getLine", Skot_getLine );
+    if( !type_engine_import_mfun( env, func ) ) goto error;
+
+    // add can_wait()
+    func = make_new_mfun( "int", "can_wait", Skot_can_wait );
+    if( !type_engine_import_mfun( env, func ) ) goto error;
+
+    // end class
+    type_engine_import_class_end( env );
+
+
+    // register deprecate
+    type_engine_register_deprecate( env, "PRC", "StringTokenizer" );
+
+    // begin class (StrTok)
+    if( !type_engine_import_class_begin( env, "StringTokenizer", "Object",
+                                         env->global(), StrTok_ctor ) )
+        return FALSE;
+
+    // add member variable
+    StrTok_offset_data = type_engine_import_mvar( env, "int", "@StrTok_data", FALSE );
+    if( StrTok_offset_data == CK_INVALID_OFFSET ) goto error;
+
+    // add set()
+    func = make_new_mfun( "void", "set", StrTok_set );
+    func->add_arg( "string", "line" );
+    if( !type_engine_import_mfun( env, func ) ) goto error;
+
+    // add reset()
+    func = make_new_mfun( "void", "reset", StrTok_reset );
+    if( !type_engine_import_mfun( env, func ) ) goto error;
+
+    // add more()
+    func = make_new_mfun( "int", "more", StrTok_more );
+    if( !type_engine_import_mfun( env, func ) ) goto error;
+
+    // add next()
+    func = make_new_mfun( "string", "next", StrTok_next );
+    if( !type_engine_import_mfun( env, func ) ) goto error;
+
+    // add get()
+    func = make_new_mfun( "string", "next", StrTok_next2 );
+    func->add_arg( "string", "out" );
+    if( !type_engine_import_mfun( env, func ) ) goto error;
+
+    // add get()
+    func = make_new_mfun( "string", "get", StrTok_get );
+    func->add_arg( "int", "index" );
+    if( !type_engine_import_mfun( env, func ) ) goto error;
+
+    // add get()
+    func = make_new_mfun( "string", "get", StrTok_get2 );
+    func->add_arg( "int", "index" );
+    func->add_arg( "string", "out" );
+    if( !type_engine_import_mfun( env, func ) ) goto error;
+
+    // add size()
+    func = make_new_mfun( "int", "size", StrTok_size );
+    if( !type_engine_import_mfun( env, func ) ) goto error;
+
+    // end class
+    type_engine_import_class_end( env );
+
+
 #ifdef AJAY
 
     // begin class
@@ -345,89 +438,6 @@ DLL_QUERY libstd_query( Chuck_DL_Query * QUERY )
     // end the class import
     type_engine_import_class_end( env );
 
-
-    // begin class (Skot)
-    if( !type_engine_import_class_begin( env, "Skot", "Event",
-                                         env->global(), Skot_ctor ) )
-        return FALSE;
-
-    // add member variable
-    Skot_offset_data = type_engine_import_mvar( env, "int", "@Skot_data", FALSE );
-    if( Skot_offset_data == CK_INVALID_OFFSET ) goto error;
-
-    // add prompt()
-    func = make_new_mfun( "Event", "prompt", Skot_prompt );
-    if( !type_engine_import_mfun( env, func ) ) goto error;
-
-    // add prompt()
-    func = make_new_mfun( "Event", "prompt", Skot_prompt2 );
-    func->add_arg( "string", "what" );
-    if( !type_engine_import_mfun( env, func ) ) goto error;
-
-    // add ready()
-    func = make_new_mfun( "int", "more", Skot_more );
-    if( !type_engine_import_mfun( env, func ) ) goto error;
-
-    // add getString()
-    func = make_new_mfun( "string", "getLine", Skot_getLine );
-    if( !type_engine_import_mfun( env, func ) ) goto error;
-
-    // add can_wait()
-    func = make_new_mfun( "int", "can_wait", Skot_can_wait );
-    if( !type_engine_import_mfun( env, func ) ) goto error;
-
-    // end class
-    type_engine_import_class_end( env );
-
-
-    // begin class (StrTok)
-    if( !type_engine_import_class_begin( env, "PRC", "Object",
-                                         env->global(), StrTok_ctor ) )
-        return FALSE;
-
-    // add member variable
-    StrTok_offset_data = type_engine_import_mvar( env, "int", "@StrTok_data", FALSE );
-    if( StrTok_offset_data == CK_INVALID_OFFSET ) goto error;
-
-    // add set()
-    func = make_new_mfun( "void", "set", StrTok_set );
-    func->add_arg( "string", "line" );
-    if( !type_engine_import_mfun( env, func ) ) goto error;
-
-    // add reset()
-    func = make_new_mfun( "void", "reset", StrTok_reset );
-    if( !type_engine_import_mfun( env, func ) ) goto error;
-
-    // add more()
-    func = make_new_mfun( "int", "more", StrTok_more );
-    if( !type_engine_import_mfun( env, func ) ) goto error;
-
-    // add next()
-    func = make_new_mfun( "string", "next", StrTok_next );
-    if( !type_engine_import_mfun( env, func ) ) goto error;
-
-    // add get()
-    func = make_new_mfun( "string", "next", StrTok_next2 );
-    func->add_arg( "string", "out" );
-    if( !type_engine_import_mfun( env, func ) ) goto error;
-
-    // add get()
-    func = make_new_mfun( "string", "get", StrTok_get );
-    func->add_arg( "int", "index" );
-    if( !type_engine_import_mfun( env, func ) ) goto error;
-
-    // add get()
-    func = make_new_mfun( "string", "get", StrTok_get2 );
-    func->add_arg( "int", "index" );
-    func->add_arg( "string", "out" );
-    if( !type_engine_import_mfun( env, func ) ) goto error;
-
-    // add size()
-    func = make_new_mfun( "int", "size", StrTok_size );
-    if( !type_engine_import_mfun( env, func ) ) goto error;
-
-    // end class
-    type_engine_import_class_end( env );
 
 #if defined(__PLATFORM_WIN32__)
     // begin class (Cereal)
@@ -963,276 +973,6 @@ CK_DLL_MFUN( KBHit_can_wait )
 
 
 
-#ifdef AJAY
-
-
-class ColumnReader
-{
-public:
-    ColumnReader();
-    virtual ~ColumnReader();
-
-    bool init( const string & filename, long col );
-    
-    bool reset() { if( !fin.good() ) return false; where = 0; return true; }
-    bool seek( long pos ) { if( pos < 0 || pos >= values.size() ) return false; where = pos; return true; }
-    bool more() { return where < values.size(); }
-    double curr();
-    bool next() { if( where < values.size() ) where++; return more(); }
-    long pos() { return where; }
-    long size() { return values.size(); }
-    string name() { return n; }
-
-    Chuck_String s;
-
-protected:
-    bool get_str( string & out );
-    bool get_double( double & out );
-
-protected:
-    ifstream fin;
-    string n;
-    long column;
-    long where;
-    char line[0x1000];
-    long len;
-
-    vector<double> values;
-};
-
-
-ColumnReader::ColumnReader()
-{
-    n = "[NONE]";
-    column = -1;
-    where = -1;
-    len = 0x1000;
-}
-
-
-ColumnReader::~ColumnReader()
-{
-    // close file
-    if( fin.good() ) 
-        fin.close();
-}
-
-bool ColumnReader::init( const string & filename, long col )
-{
-    // hmm
-    if( col < 1 )
-    {
-        cerr << "column must be greater than 0!!!" << endl;
-        return false;
-    }
-
-    // open file
-    fin.open( filename.c_str(), ios::in );
-    // yes
-    if( !fin.good() )
-    {
-        cerr << "ColumnReader: cannot open file: '" << filename << "'..." << endl;
-        return false;
-    }
-
-    // set column
-    column = col;
-
-    // read first line
-    if( !fin.getline( line, len ) )
-    {
-        cerr << "ColumnReader: cannot read first line: '" << filename << "'..." << endl;
-        return false;
-    }
-
-    // get the name
-    if( !get_str( n ) )
-    {
-        cerr << "ColumnReader: cannot seek to column " << col << ": " << filename << "..." << endl;
-        return false;
-    }
-
-    double v;
-    long i = 1;
-    // read values
-    while( fin.getline( line, len ) )
-    {
-        v = 0.0;
-        // get value
-        if( !get_double( v ) )
-        {
-            cerr << "ColumnReader: cannot read column " << v << " on line i: " << n << "..." << endl;
-            return false;
-        }
-
-        values.push_back( v );
-    }
-
-    // well
-    if( values.size() == 0 )
-    {
-        cerr << "ColumnReader: file doesn't not contain data after first line: " << n << "..." << endl;
-        return false;
-    }
-
-    // set location
-    where = 0;
-
-    s.str = n;
-
-    return true;
-}
-
-double ColumnReader::curr()
-{
-    if( where >= values.size() )
-    {
-        cerr << "ColumnReader: trying to read beyond end of file: " << n << "..." << endl;
-        return 0.0;
-    }
-
-    return values[where];
-}
-
-bool ColumnReader::get_double( double & out )
-{
-    assert( column > 0 );
-    long c = 1;
-    
-    char * start = line;
-    char * curr = start;
-
-    while( c < column )
-    {
-        // move past value
-        while( *curr && *curr != ',' ) curr++;
-        // move past ,
-        while( *curr && *curr == ',' ) curr++;
-        // check
-        if( *curr == '\0' )
-        {
-            cerr << "ColumnReader: cannot find column " << column << ": " << n << endl;
-            return false;
-        }
-
-        // increment
-        c++;
-        // set start
-        start = curr;
-    }
-
-    // move past value
-    while( *curr && *curr != ',' ) curr++;
-    // end
-    *curr = '\0';
-
-    out = atof( start );
-    
-    return true;
-}
-
-
-bool ColumnReader::get_str( string & out )
-{
-    assert( column > 0 );
-    long c = 1;
-    
-    char * start = line;
-    char * curr = start;
-
-    while( c < column )
-    {
-        // move past value
-        while( *curr && *curr != ',' ) curr++;
-        // move past ,
-        while( *curr && *curr == ',' ) curr++;
-        // check
-        if( *curr == '\0' )
-        {
-            cerr << "ColumnReader: cannot find column " << column << ": " << n << endl;
-            return false;
-        }
-
-        // increment
-        c++;
-        // set start
-        start = curr;
-    }
-
-    // move past value
-    while( *curr && *curr != ',' ) curr++;
-    // end
-    *curr = '\0';
-
-    out = start;
-    
-    return true;
-}
-
-
-CK_DLL_CTOR( VCR_ctor )
-{
-    OBJ_MEMBER_INT(SELF, VCR_offset_data) = (t_CKUINT)new ColumnReader;
-}
-
-CK_DLL_MFUN( VCR_load )
-{
-    ColumnReader * vcr = (ColumnReader*)OBJ_MEMBER_INT(SELF, VCR_offset_data);
-    const char * filename = GET_NEXT_STRING(ARGS)->str.c_str();
-    t_CKINT column = GET_NEXT_INT(ARGS);
-    RETURN->v_int = vcr->init( filename, column );
-}
-
-CK_DLL_MFUN( VCR_reset )
-{
-    ColumnReader * vcr = (ColumnReader*)OBJ_MEMBER_INT(SELF, VCR_offset_data);
-    RETURN->v_int = vcr->reset();
-}
-
-CK_DLL_MFUN( VCR_seek )
-{
-    ColumnReader * vcr = (ColumnReader*)OBJ_MEMBER_INT(SELF, VCR_offset_data);
-    t_CKINT where = GET_CK_INT(ARGS);
-    RETURN->v_int = vcr->seek( where );
-}
-
-CK_DLL_MFUN( VCR_more )
-{
-    ColumnReader * vcr = (ColumnReader*)OBJ_MEMBER_INT(SELF, VCR_offset_data);
-    RETURN->v_int = vcr->more();
-}
-
-CK_DLL_MFUN( VCR_curr )
-{
-    ColumnReader * vcr = (ColumnReader*)OBJ_MEMBER_INT(SELF, VCR_offset_data);
-    RETURN->v_float = vcr->curr();
-}
-
-CK_DLL_MFUN( VCR_next )
-{
-    ColumnReader * vcr = (ColumnReader*)OBJ_MEMBER_INT(SELF, VCR_offset_data);
-    RETURN->v_int = vcr->next();
-}
-
-CK_DLL_MFUN( VCR_pos )
-{
-    ColumnReader * vcr = (ColumnReader*)OBJ_MEMBER_INT(SELF, VCR_offset_data);
-    RETURN->v_int = vcr->pos();
-}
-
-CK_DLL_MFUN( VCR_size )
-{
-    ColumnReader * vcr = (ColumnReader*)OBJ_MEMBER_INT(SELF, VCR_offset_data);
-    RETURN->v_int = vcr->size();
-}
-
-CK_DLL_MFUN( VCR_name )
-{
-    ColumnReader * vcr = (ColumnReader*)OBJ_MEMBER_INT(SELF, VCR_offset_data);
-    RETURN->v_string = &(vcr->s);
-}
-
-
 class LineEvent : public Chuck_Event
 {
 public:
@@ -1565,6 +1305,278 @@ CK_DLL_MFUN( StrTok_size )
 {
     StrTok * tokens = (StrTok *)OBJ_MEMBER_INT(SELF, StrTok_offset_data);
     RETURN->v_int = tokens->size();
+}
+
+
+
+
+#ifdef AJAY
+
+
+class ColumnReader
+{
+public:
+    ColumnReader();
+    virtual ~ColumnReader();
+
+    bool init( const string & filename, long col );
+    
+    bool reset() { if( !fin.good() ) return false; where = 0; return true; }
+    bool seek( long pos ) { if( pos < 0 || pos >= values.size() ) return false; where = pos; return true; }
+    bool more() { return where < values.size(); }
+    double curr();
+    bool next() { if( where < values.size() ) where++; return more(); }
+    long pos() { return where; }
+    long size() { return values.size(); }
+    string name() { return n; }
+
+    Chuck_String s;
+
+protected:
+    bool get_str( string & out );
+    bool get_double( double & out );
+
+protected:
+    ifstream fin;
+    string n;
+    long column;
+    long where;
+    char line[0x1000];
+    long len;
+
+    vector<double> values;
+};
+
+
+ColumnReader::ColumnReader()
+{
+    n = "[NONE]";
+    column = -1;
+    where = -1;
+    len = 0x1000;
+}
+
+
+ColumnReader::~ColumnReader()
+{
+    // close file
+    if( fin.good() ) 
+        fin.close();
+}
+
+bool ColumnReader::init( const string & filename, long col )
+{
+    // hmm
+    if( col < 1 )
+    {
+        cerr << "column must be greater than 0!!!" << endl;
+        return false;
+    }
+
+    // open file
+    fin.open( filename.c_str(), ios::in );
+    // yes
+    if( !fin.good() )
+    {
+        cerr << "ColumnReader: cannot open file: '" << filename << "'..." << endl;
+        return false;
+    }
+
+    // set column
+    column = col;
+
+    // read first line
+    if( !fin.getline( line, len ) )
+    {
+        cerr << "ColumnReader: cannot read first line: '" << filename << "'..." << endl;
+        return false;
+    }
+
+    // get the name
+    if( !get_str( n ) )
+    {
+        cerr << "ColumnReader: cannot seek to column " << col << ": " << filename << "..." << endl;
+        return false;
+    }
+
+    double v;
+    long i = 1;
+    // read values
+    while( fin.getline( line, len ) )
+    {
+        v = 0.0;
+        // get value
+        if( !get_double( v ) )
+        {
+            cerr << "ColumnReader: cannot read column " << v << " on line i: " << n << "..." << endl;
+            return false;
+        }
+
+        values.push_back( v );
+    }
+
+    // well
+    if( values.size() == 0 )
+    {
+        cerr << "ColumnReader: file doesn't not contain data after first line: " << n << "..." << endl;
+        return false;
+    }
+
+    // set location
+    where = 0;
+
+    s.str = n;
+
+    return true;
+}
+
+double ColumnReader::curr()
+{
+    if( where >= values.size() )
+    {
+        cerr << "ColumnReader: trying to read beyond end of file: " << n << "..." << endl;
+        return 0.0;
+    }
+
+    return values[where];
+}
+
+bool ColumnReader::get_double( double & out )
+{
+    assert( column > 0 );
+    long c = 1;
+    
+    char * start = line;
+    char * curr = start;
+
+    while( c < column )
+    {
+        // move past value
+        while( *curr && *curr != ',' ) curr++;
+        // move past ,
+        while( *curr && *curr == ',' ) curr++;
+        // check
+        if( *curr == '\0' )
+        {
+            cerr << "ColumnReader: cannot find column " << column << ": " << n << endl;
+            return false;
+        }
+
+        // increment
+        c++;
+        // set start
+        start = curr;
+    }
+
+    // move past value
+    while( *curr && *curr != ',' ) curr++;
+    // end
+    *curr = '\0';
+
+    out = atof( start );
+    
+    return true;
+}
+
+
+bool ColumnReader::get_str( string & out )
+{
+    assert( column > 0 );
+    long c = 1;
+    
+    char * start = line;
+    char * curr = start;
+
+    while( c < column )
+    {
+        // move past value
+        while( *curr && *curr != ',' ) curr++;
+        // move past ,
+        while( *curr && *curr == ',' ) curr++;
+        // check
+        if( *curr == '\0' )
+        {
+            cerr << "ColumnReader: cannot find column " << column << ": " << n << endl;
+            return false;
+        }
+
+        // increment
+        c++;
+        // set start
+        start = curr;
+    }
+
+    // move past value
+    while( *curr && *curr != ',' ) curr++;
+    // end
+    *curr = '\0';
+
+    out = start;
+    
+    return true;
+}
+
+
+CK_DLL_CTOR( VCR_ctor )
+{
+    OBJ_MEMBER_INT(SELF, VCR_offset_data) = (t_CKUINT)new ColumnReader;
+}
+
+CK_DLL_MFUN( VCR_load )
+{
+    ColumnReader * vcr = (ColumnReader*)OBJ_MEMBER_INT(SELF, VCR_offset_data);
+    const char * filename = GET_NEXT_STRING(ARGS)->str.c_str();
+    t_CKINT column = GET_NEXT_INT(ARGS);
+    RETURN->v_int = vcr->init( filename, column );
+}
+
+CK_DLL_MFUN( VCR_reset )
+{
+    ColumnReader * vcr = (ColumnReader*)OBJ_MEMBER_INT(SELF, VCR_offset_data);
+    RETURN->v_int = vcr->reset();
+}
+
+CK_DLL_MFUN( VCR_seek )
+{
+    ColumnReader * vcr = (ColumnReader*)OBJ_MEMBER_INT(SELF, VCR_offset_data);
+    t_CKINT where = GET_CK_INT(ARGS);
+    RETURN->v_int = vcr->seek( where );
+}
+
+CK_DLL_MFUN( VCR_more )
+{
+    ColumnReader * vcr = (ColumnReader*)OBJ_MEMBER_INT(SELF, VCR_offset_data);
+    RETURN->v_int = vcr->more();
+}
+
+CK_DLL_MFUN( VCR_curr )
+{
+    ColumnReader * vcr = (ColumnReader*)OBJ_MEMBER_INT(SELF, VCR_offset_data);
+    RETURN->v_float = vcr->curr();
+}
+
+CK_DLL_MFUN( VCR_next )
+{
+    ColumnReader * vcr = (ColumnReader*)OBJ_MEMBER_INT(SELF, VCR_offset_data);
+    RETURN->v_int = vcr->next();
+}
+
+CK_DLL_MFUN( VCR_pos )
+{
+    ColumnReader * vcr = (ColumnReader*)OBJ_MEMBER_INT(SELF, VCR_offset_data);
+    RETURN->v_int = vcr->pos();
+}
+
+CK_DLL_MFUN( VCR_size )
+{
+    ColumnReader * vcr = (ColumnReader*)OBJ_MEMBER_INT(SELF, VCR_offset_data);
+    RETURN->v_int = vcr->size();
+}
+
+CK_DLL_MFUN( VCR_name )
+{
+    ColumnReader * vcr = (ColumnReader*)OBJ_MEMBER_INT(SELF, VCR_offset_data);
+    RETURN->v_string = &(vcr->s);
 }
 
 
